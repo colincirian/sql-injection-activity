073c226384d9315089dd6b45d2a9a43d257fecdf
Colin <colindcirian@gmail.com>
2023-07-31 17:46:24 -0700

    finished

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.json())
 
 const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
- db.run(â€œCREATE TABLE user (username TEXT, password TEXT, title TEXT)â€œ);
- db.run(â€œINSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')â€œ);
+    db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+    db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
 });
 
 app.get('/', (req, res) => {
@@ -19,21 +19,23 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+	const query = "SELECT title FROM user where username = '" + username + "' and password = '" + password + "'";
 
-});
-
-db.get(query, function (err, row) {
-	if (err) {
-		console.log('ERROR', err);
-		res.redirect(â€œ/index.html#errorâ€);
-	} else if (!row) {
-		res.redirect(â€œ/index.html#unauthorizedâ€);
-	} else {
-		res.send('Hello <b>' + row.title + '!</b><br /> 
-		This file contains all your secret data: <br /><br /> 
-		SECRETS <br /><br /> MORE SECRETS <br /><br /> 
-		<a href=â€œ/index.htmlâ€>Go back to login</a>');
-	}
+	db.get(query, function (err, row) {
+		if (err) {
+			console.log('ERROR', err);
+			res.redirect('/index.html#error');
+		} else if (!row) {
+			res.redirect('/index.html#unauthorized');
+		} else {
+			res.send('Hello <b>' + row.title + '!</b><br />' +
+					'This file contains all your secret data: <br /><br />' +
+					'SECRETS <br /><br /> MORE SECRETS <br /><br />' +
+					'<a href="/index.html">Go back to login</a>');
+		}
+	});
 });
 
 
